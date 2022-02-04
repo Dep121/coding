8dce7c7255fc496610f6de944ab29fca2d8cb467
dep121 <deepakkumarpandey121@gmail.com>
2022-02-04 15:35:15 +0530

    Priority queue and countSetbits

--- JSDS/PriorityQueue.js
+++ JSDS/PriorityQueue.js
@@ -17,8 +17,8 @@ class PriorityQueue { // using max heap
 
   heapify(r) {
     let largest = r;
-    let left = r * 2;
-    let right = r * 2 + 1;
+    let left = r * 2 + 1;
+    let right = r * 2 + 2;
 
     if (left < this.size && this.heap[largest] < this.heap[left]) {
       largest = left;
@@ -45,9 +45,14 @@ class PriorityQueue { // using max heap
     for (let i = Math.floor(this.size / 2) - 1; i>=0; --i) {
       this.heapify(i);
     }
+    console.log(`----> ${this.heap}`);
   }
 
   delete(data) {
+    if (!Number.isInteger(data)) {
+      console.log(`invalid number ${data}`);
+      return;
+    }
     if (this.isEmpty()) {
       console.log(`heap empty...`);
       return;
@@ -87,25 +92,28 @@ class PriorityQueue { // using max heap
   }
 }
 
-const pq = new PriorityQueue(10);
+function main() {
+  const pq = new PriorityQueue(10);
 
-pq.insert(9);
-pq.insert(3);
-pq.insert(5);
-pq.insert(1);
-pq.insert(4);
-pq.insert(2);
-pq.insert(7);
-pq.display();
+  pq.insert(9);
+  pq.insert(3);
+  pq.insert(5);
+  pq.insert(1);
+  pq.insert(4);
+  pq.insert(2);
+  pq.insert(7);
 
-pq.delete(3);
-pq.display();
+  pq.display();
 
+  pq.delete ();
 
+  pq.display();
+}
+
+main();
+
+// my output:
 //       9
 //   5       4
 // 1   3   2   7
 
-//       9
-//   7       5
-// 1   4   2
